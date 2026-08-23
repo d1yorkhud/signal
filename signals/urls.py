faae52c340eb5a547c4b56970020e557d68e2e59
d1yorkhud <d1yorkhud@gmail.com>
2026-08-23 21:32:42 +0900

    fixing contact page url error

--- signals/urls.py
+++ signals/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import news_list, news_detail, homeView
+from .views import news_list, news_detail, homeView, contactPageView
 
 urlpatterns = [
     path('', homeView, name='home_page'),
     path('news/', news_list, name="all_news_list"),
     path('news/<int:id>/', news_detail, name="news_detail_page"),
+    path('contact/', contactPageView, name='contact_page'),
 ]
