79df85c622f2e41ccc30b2ad1a287a94129dfece
d1yorkhud <d1yorkhud@gmail.com>
2026-09-15 01:04:36 +0900

    fixin title issues

--- signals/urls.py
+++ signals/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import news_list, news_detail, homeView, ContactPageView, aboutusPageView, categoryPageView, HomePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home_page'),
+    path('', homeView, name='home_page'),
     path('news/', news_list, name="all_news_list"),
     path('news/<int:id>/', news_detail, name="news_detail_page"),
     path('contact/', ContactPageView.as_view(), name='contact_page'),
